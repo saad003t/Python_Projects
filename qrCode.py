169df9ba1c99974a086279d375d30ccda72202a4
Khan Mohammad Saad <kmsaad148@gmail.com>
2026-09-21 07:51:13 +0600

    Add files via upload

---
+++ qrCode.py
@@ -0,0 +1,12 @@
+import qrcode 
+from PIL import Image
+
+qr = qrcode.QRCode(version = 1,
+                   error_correction = qrcode.constants.ERROR_CORRECT_H,
+                   box_size = 10,
+                   border = 4)
+
+qr.add_data("https://www.linkedin.com/in/khan-mohammad-saad/")
+qr.make(fit = True)
+img = qr.make_image(fill_color = "white", back_color = "black")
+img.save("linkedin_qr.png")
